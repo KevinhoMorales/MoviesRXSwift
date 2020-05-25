21fc569a792018c6f74de7aedd80a6107492ea6a
Kevinho Morales <kfmorales94@gmail.com>
2020-05-25 11:44:07 -0500

    Initial Commit

---
+++ Movies/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Movies
+//
+//  Created by Kevinho Morales on 5/25/20.
+//  Copyright © 2020 Kevinho Morales. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
